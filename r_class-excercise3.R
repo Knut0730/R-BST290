b70e881008dcba6c2b885e5d90c42528f969efca
Knut0730 <knuti992@icloud.com>
2022-03-23 20:41:06 +0100

    changed some small things

--- r_class-excercise3.R
+++ r_class-excercise3.R
@@ -2,6 +2,7 @@
 
 library(haven)
 library(bst290)
+
 ucdp <- read.csv("https://data.humdata.org/dataset/bb30eaae-306d-4d53-8978-998eb8b9a06e/resource/d4c483d4-e8dc-4246-9b6c-3aef8a7b16c6/download/conflict_data_ukr.csv")
 ucdp <- ucdp[-1,] # Tidies up the dataset, removes the first row
 
--- r_exercise3.R
+++ r_exercise3.R
@@ -18,16 +18,24 @@ mean(cpds$womenpar, na.rm=TRUE) # removes NA
 median(cpds$womenpar)
 median(cpds$womenpar, na.rm = TRUE)
 
+var(cpds$womenpar, na.rm = TRUE)
+sd(cpds$womenpar, na.rm = TRUE)
 # Without removing na.rm there is no data (NA), but by removing NA we can see that in parliaments women are about 13%.
 
 
 ## Task 4 ####
-oppsumtabell(dataset= cpds, variables = "womenpar")
+oppsumtabell(dataset= cpds,
+             variables = "womenpar")
   # 1745 observations / 16.45% is average representation / lower 25% is 6% / median is 13 aka. 50% / 75% ligger på 24%. | Noen parliamenter har 0 representation og den høyeste har 47.60.
 
 unique(cpds$eu) # 1 means EU member, 0 means NON-EU.
-oppsum_grupp(dataset = cpds, variable = "womenpar", by.var = "eu") # EU countries are a little bit better.
-oppsum_grupp(dataset = cpds, variable = "womenpar", by.var = "country") # Iceland is the country with most representation 47.60 % / Norway is 41.40 % / 
+oppsum_grupp(dataset = cpds, 
+             variable = "womenpar", 
+             by.var = "eu") # EU countries are a little bit better.
+
+oppsum_grupp(dataset = cpds, 
+             variable = "womenpar",
+             by.var = "country") # Iceland is the country with most representation 47.60 % / Norway is 41.40 % / 
 
 ### DO DEBUGGING NEXT
 # Full score
