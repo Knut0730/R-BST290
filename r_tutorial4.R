6e0e8db9685e2b4417077dd142e9c6ba2868085a
Knut0730 <knuti992@icloud.com>
2022-03-26 14:57:07 +0100

    Done tutorial until task 3

--- r_tutorial4.R
+++ r_tutorial4.R
@@ -17,7 +17,23 @@ ess_gndr_age <- select(.data = ess, agea, gndr)
 # Removes Age and Gender from the ESS dataset.
 select(.data = ess, -agea, gndr)
 
+# Filters out people aged 40 or older. (Removes everone under 40.)
+filter(.data = ess, agea>=40)
+  # > “greater”
+  # < “smaller”
+  # <= “smaller or equal to”
+  # >= “greater or equal to”
+  # == “has to be equal to” (the double equal sign means we are extra sure here)
+  # != “must not not be equal to” (generally, ! stands for “is not”)
+  # %in% “is included in”
+
+# Filters out women who are older than 35.
+filter(.data = ess, agea > 35 & gndr == "Female")
+
 # Ask Carlo why this does not work? - Want to visualize with a histogram.
 Movie <- c("The Truman Show", "Don't look up", "The Do-Over", "The Dictator", "Red Notice", "Eurovision Song Contest", "Central Intelligence")
 Result <- c(4, 3, 0, 3, 2, 1, 0)
 df <- data.frame(Movie, Result)
+
+## 4 ####
+
