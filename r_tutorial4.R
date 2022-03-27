f16196ccab0684a95a6b5ff373947ff1cdc66e37
Knut0730 <knuti992@icloud.com>
2022-03-27 19:43:35 +0200

    Done task 5 and started on 6.

--- r_tutorial4.R
+++ r_tutorial4.R
@@ -42,3 +42,31 @@ ess_female_old <- ess %>%
   select(idno, agea, gndr) %>%
   filter(gndr == "Female" & agea > 35)
 # Shortcut for pipe operator: CTRL + SHIFT + P %>% 
+
+## 5 ####
+
+# Height summary
+summary(ess$height)
+
+# Mutate height from cm to m
+ess %>% 
+  mutate(height_meters = height/100) #/100 divides height by 100.
+
+# if_else with age older than 65. | ifelse also exists, but if_else is made for mutate() and more strict. - less errors
+ess %>% 
+  select(agea) %>% # Used to only output Age - decluttering
+  mutate(older = if_else(condition = agea > 65, # Checks if older than 65
+                         true = 1, # 1 - True
+                         false = 0)) # 0 - False
+
+## 6 ####
+
+# Outputs the mean height, and removing NA's (normal way)
+mean(ess$height, na.rm = T)
+
+# Outputs the mean height, and removing NA's (tidyverse way)
+ess %>% 
+summarize(mean_height = mean(height, na.rm = T))
+mean_height
+
+# page 10.
