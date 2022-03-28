12e0fd141bf336e42411e95a04131025b61896c6
Knut0730 <knuti992@icloud.com>
2022-03-28 14:18:58 +0200

    Finished tutorial

--- r_tutorial4.R
+++ r_tutorial4.R
@@ -69,4 +69,59 @@ ess %>%
 summarize(mean_height = mean(height, na.rm = T))
 mean_height
 
-# page 10.
+# Ess -> groups by gender -> summarizes mean height from male and female (179-168)
+ess %>% 
+  group_by(gndr) %>% 
+  summarize(mean_height = mean(height, na.rm = T))
+
+# ESS -> groups by gender -> summarizes median age from male and female (51-42.5)
+ess %>% 
+  group_by(gndr) %>% 
+  summarize(med_age = median(agea, na.rm = T))
+
+# ESS -> groups by gender -> summarizes the N of each gender
+ess %>% 
+  group_by(gndr) %>% 
+  summarize(obs = n())
+
+# Multiple summary; obs = N; med_age = median age; mean_weight = average weight.
+ess %>% 
+  group_by(gndr) %>% 
+  summarize(obs = n(),
+            med_age = median(agea, na.rm = T),
+            mean_weight = mean(weight, na.rm = T))
+
+## 7 ####
+
+# Different ways of seeing the class of a variable (character, numeric, factor...)
+class(ess$cntry)
+typeof(ess$cntry)
+mode(ess$cntry)
+
+# Stores the agea variable as chr instead of numeric.
+ess$agea_chr.silly <- as.character(ess$agea)
+ess$agea_chr.silly
+
+# You can not calculate the mean of a character variable
+mean(ess$agea_chr.silly, na.rm = T)
+
+# Changes character variable to numeric using as.numeric().
+ess$age_num <- as.numeric(ess$agea_chr.silly)
+mean(ess$age_num, na.rm = T)
+
+# Converting factor variable to numeric
+mean(ess$stflife, na.rm = T) # does not work because of factor
+class(ess$stflife) # Confirms that it is a factor variable 
+ess$stflife_num <- as.numeric(ess$stflife) # Makes new variable which is numeric
+unique(ess$stflife_num) # Checks how many unique observations there are.
+summary(ess$stflife_num) # Summarizes
+table(ess$stflife, ess$stflife_num) # Cross table of factor stflife and numerical stflife
+ess$stflife_num <- ess$stflife_num - 1 # Needed to fix that R converted the 0-10 variable to 1-11
+mean(ess$stflife_num, na.rm = T) # You can now calculate the mean of the before factor variable.
+
+# Converts factor variable into character
+ess$edlv_chr <- as.character(ess$edlvdno)
+class(ess$edlv_chr)
+unique(ess$edlv_chr)
+
+# forcats package for working with factors a lot.
